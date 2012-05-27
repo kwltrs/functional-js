fefdc2045fe67050d1d137f5edd202fd53cd563b
Kristofer <kris@wltrs.org>
2012-05-27 17:23:09 +0200

    Indent with spaces, not tabs.

--- buster.js
+++ buster.js
@@ -1,7 +1,7 @@
 var config = module.exports;
 
 config["Tests"] =  {
-	env: "node",
-	sources: ["spike.js"],
-	tests: ["test/spike-test.js"]
+    env: "node",
+    sources: ["spike.js"],
+    tests: ["test/spike-test.js"]
 };
